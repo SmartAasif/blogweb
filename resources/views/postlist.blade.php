29c71bc69f492c2120bd866ea4229cf529ed2c94
aasif <121aasifali@gmail.com>
2021-03-10 15:45:41 +0530

    crud in ubold with modal

--- resources/views/postlist.blade.php
+++ resources/views/postlist.blade.php
@@ -10,8 +10,8 @@
             <div class="page-title-box">
                 <div class="page-title-right">
                     <ol class="breadcrumb m-0">
-                        <li class="breadcrumb-item"><a href="javascript: void(0);">UBold</a></li>
-                        <li class="breadcrumb-item"><a href="javascript: void(0);">Tables</a></li>
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">Posts</a></li>
+                        <li class="breadcrumb-item"><a href="javascript: void(0);">Table</a></li>
                         <li class="breadcrumb-item active">Foo</li>
                     </ol>
                 </div>
